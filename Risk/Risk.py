7499bef12c77d26a44003986f26ab3b7d4b9b140
florentvx <34220583+florentvx@users.noreply.github.com>
2018-01-19 01:50:23 +0000

    Updated Density class
    Method to transform the initial historical returns to the equivalent std normal ones

--- Risk/Risk.py
+++ Risk/Risk.py
@@ -1,7 +1,9 @@
 from Prices import Currency
 from Requests import OHLCLibrary
 from scipy import stats
-from pandas import DataFrame
+import pandas as pd
+import math as mth
+import matplotlib.pyplot as plt
 
 class Density:
 
@@ -10,16 +12,29 @@ class Density:
         self.CurrencyRef = curRef
         self.Fixing = fixing
         self.Freq = freq
+        self.Points = []
 
     def DownloadReturns(self):
-        DF = DataFrame()
+        DF = pd.DataFrame()
         DF = OHLCLibrary(self.Currency, self.CurrencyRef, freq = self.Freq, isUpdating = False)
         DF["Return"] = DF[self.Fixing]/DF[self.Fixing].shift(1) - 1
-        self.DataFrame = DF
+        self.Returns = list(DF["Return"][1:])
 
-    def GetPDF(self, max: float = 0.25, n: int = 100):
-        self.Points = [-max + 2 * max * i / float(n) for i in range(n + 1)]
-        self.PDF = stats.gaussian_kde(self.DataFrame["Return"][1:]).evaluate(self.Points)
+    def GetPDF(self, max: float = 0.25, n: int = 1000):
+        self.dx = 2 * max / float(n)
+        self.Max = max
+        self.Points = [-max + self.dx * i  for i in range(n + 1)]
+        self.PDF = stats.gaussian_kde(self.Returns).evaluate(self.Points)
+
+    def ToDataFrame(self):
+        DF = pd.DataFrame()
+        try:
+            DF["Points"] = self.Points
+            DF["PDF"] = self.PDF
+            return DF
+        except:
+            return pd.DataFrame()
+
 
     def GetCDF(self, precision : int = 100):
         CDF = []
@@ -32,17 +47,65 @@ class Density:
             dtot = 0
             while tot + dtot < x:
                 tot += dtot
-                dtot = (self.Points[i+1] - self.Points[i]) * self.PDF[i]
+                dtot = self.dx * self.PDF[i]
                 i += 1
             w = (x - tot) / dtot
-            CDF += [(1 - w) * self.Points[i] + w * (self.Points[i + 1])]
+            CDF += [self.Points[i - 2] - w * self.dx]
+            x += dx
         self.CDF = CDF
 
-d = Density(Currency.XBT)
-d.DownloadReturns()
-d.GetPDF()
-d.GetCDF(2)
+    def GetPDFMeasure(self, n: int):
+        res = 0
+        for i in range(len(self.Points)):
+            res += self.PDF[i] * (self.Points[i]) ** n
+        return res * self.dx
 
+    def NormParam(self):
+        mu = self.GetPDFMeasure(1)
+        return (mu, (self.GetPDFMeasure(2) - mu ** 2) ** 0.5)
+    
+    def GetHistoricalQuantiles(self):
+        Quantiles = []
+        n = float(len(self.CDF))
+        for ret in self.Returns:
+            i = 0
+            while i < int(n) and self.CDF[i] < ret:
+                i += 1
+            if i == 0:
+                w = (ret + self.Max) / (self.CDF[i] + self.Max)
+                Quantiles += [w / n]
+            elif i == n:
+                w = (ret - self.Max) / (self.CDF[i-1] - self.Max)
+                Quantiles += [1 - w / n]
+            else:
+                w = (ret - self.CDF[i]) / (self.CDF[i-1] - self.CDF[i])
+                Quantiles += [(i + 1 - w) / n]
+        self.Quantiles = Quantiles
+
+    def TransformStdNorm(self):
+        StdReturns = []
+        for q in self.Quantiles:
+            StdReturns += [stats.norm.ppf(q)]
+        self.StdReturns = StdReturns
+
+    def TotalTransform(self):
+        self.DownloadReturns()
+        print("Loaded " + str(len(self.Returns)) + " Returns for the Currency: " + self.Currency.ToString)
+        self.GetPDF()
+        self.GetCDF()
+        self.GetHistoricalQuantiles()
+        self.TransformStdNorm()
+
+
+d = Density(Currency.XBT)
+d.TotalTransform()
+
+plt.figure()
+plt.subplot(211)
+plt.hist(d.Returns, bins = 50)
+plt.subplot(212)
+plt.hist(d.StdReturns, bins = 50)
+plt.show()
 
 
 
