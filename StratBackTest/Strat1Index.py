0e60be751f3dcf8974244376e38e2b38e366638d
florentvx <34220583+florentvx@users.noreply.github.com>
2018-04-22 14:01:28 +0100

    Strat Update
    Added the Volatility Weighted Moving Average

--- StratBackTest/Strat1Index.py
+++ StratBackTest/Strat1Index.py
@@ -1,12 +1,11 @@
 from matplotlib import pyplot as plt
-from dask.dataframe.rolling import rolling_mean
-from pandas import DataFrame
+import pandas as pd
 from numpy import int, str, float
 from Prices import Currency
 from Requests import OHLCLibrary
 from scipy import stats
+from math import sqrt
 import numpy as np
-
 
 class Strat1Index:
     def __init__(self, curID: str, currefID: str, fixing: str = "close", freq: int = 240):
@@ -16,33 +15,52 @@ class Strat1Index:
         self.Freq = freq
 
     def Download(self):
-        DF = DataFrame()
+        DF = pd.DataFrame()
         DF = OHLCLibrary(self.Cur, self.CurRef, freq = self.Freq)
         DF["Return"] = DF[self.Fixing]/DF[self.Fixing].shift(1) - 1
         self.DataFrame = DF
 
     def MovingAverage(self, freq: int):
-        self.DataFrame["MA_" + str(freq)] = rolling_mean(self.DataFrame[self.Fixing], freq)
+        self.DataFrame["MA_" + str(freq)] = pd.rolling_mean(self.DataFrame[self.Fixing], freq)
 
+    def VWMA(self, freq: int):
+        VWMAcolumn = "VWMA_" + str(freq)
+        self.DataFrame[VWMAcolumn] = self.DataFrame[self.Fixing] * self.DataFrame["volume"]
+        self.DataFrame["temp"] = self.DataFrame["volume"].rolling(window = freq).sum()
+        self.DataFrame[VWMAcolumn] = self.DataFrame[VWMAcolumn].rolling(window = freq).sum()
+        self.DataFrame[VWMAcolumn] = self.DataFrame.apply(lambda row: row[VWMAcolumn] / row["temp"], axis = 1)
+
+    def Results(self, col: str = "temp"):
+        average = self.DataFrame[col].mean() * (240 * 6) /self.Freq * 30
+        stddev = self.DataFrame[col].std() * sqrt((240 * 6) / self.Freq * 30)
+        sharpe = average/stddev
+        return [average, stddev,sharpe]
+
+    def ResultString(self, col: str = "temp"):
+        L = self.Results(col)
+        res = "The average (monthy) return is: " + str(round(L[0] * 100, 4)) + "% \n"
+        res += "The average (monthly) return is: " + str(round(L[1] * 100, 4)) + "% \n"
+        res += "The Sharpe Ratio is: " + str(round(L[2], 4)) + " \n"
+        return res
+        
     def ApplyAllocation(self):
         self.DataFrame["temp"] = self.DataFrame.apply(lambda row: row["Return"] * row["Alloc"], axis = 1)
         Index = [1000]
         i=0
-        print(self.DataFrame)
         for (index,row) in self.DataFrame[1:].iterrows():
             i+=1
             Index += [Index[-1] * (1 + row["temp"])]
         self.DataFrame["Index"] = Index
+        
 
-
-    def SimpleMAStrat(self, freq1 : int, freq2: int, factor : float = 1, longOnly: bool = True):
-        self.MovingAverage(freq1)
-        self.MovingAverage(freq2)
+    def SimpleMAStrat(self, freq1 : int, freq2: int, factor : float = 1, longOnly: bool = False):
+        self.VWMA(freq1)
+        self.VWMA(freq2)
         n = max(freq1,freq2)
         alloc = [0 for i in range(n)]
         for (index,row) in self.DataFrame[n:].iterrows():
-            ma1 = row["MA_" + str(freq1)]
-            ma2 = row["MA_" + str(freq2)]
+            ma1 = row["VWMA_" + str(freq1)]
+            ma2 = row["VWMA_" + str(freq2)]
             pos = 1 - factor * abs((ma1 - ma2)/ma2)
             if longOnly:
                 if ma1 > ma2:
@@ -58,34 +76,42 @@ class Strat1Index:
         self.ApplyAllocation()
 
 
-s1 = Strat1Index("XBT","EUR",freq = 1)
+s1 = Strat1Index("XBT","EUR",freq = 60)
 s1.Download()
 
-s1.SimpleMAStrat(5,10,50)
-#s1.DataFrame.to_clipboard()
-I1 = list(s1.DataFrame["Index"])
-A1 = list(s1.DataFrame["Alloc"])
-s1.SimpleMAStrat(5,10,100)
-I2 = list(s1.DataFrame["Index"])
-A2 = list(s1.DataFrame["Alloc"])
-s1.SimpleMAStrat(5,10)
+freqMA = [5,10,15,20]
+factors = [-1000,-100,-10,10,100,1000
+           ]
+
+param = [0,0,0]
+sMax = 0
+for i in range(1,len(freqMA)):
+    print(i)
+    for j in range(i):
+        MA1 = freqMA[j]
+        MA2 = freqMA[i]
+        for factor in factors:
+            s1.SimpleMAStrat(MA1,MA2,factor)
+            s = s1.Results()[2]
+            if s > sMax:
+                param = [MA1,MA2,factor]
+                sMax = s
+
+print("MA1: " + str(param[0]) + "\n MA2: " + str(param[1]) + "\n Factor " + str(param[2]))
+s1.SimpleMAStrat(param[0],param[1],param[2])
+print(s1.ResultString())
+
 
 
 plt.figure(1)
 plt.subplot(311)
 plt.plot(s1.DataFrame["time"], s1.DataFrame["close"])
-plt.plot(s1.DataFrame["time"], s1.DataFrame["MA_" + str(5)])
-plt.plot(s1.DataFrame["time"], s1.DataFrame["MA_" + str(10)])
+plt.plot(s1.DataFrame["time"], s1.DataFrame["VWMA_" + str(param[0])])
+plt.plot(s1.DataFrame["time"], s1.DataFrame["VWMA_" + str(param[1])])
 
 plt.subplot(312)
-plt.plot(s1.DataFrame["time"], I1)
-plt.plot(s1.DataFrame["time"], I2)
 plt.plot(s1.DataFrame["time"], s1.DataFrame["Index"])
-plt.legend([str(50),str(100),str(1)])
 
 plt.subplot(313)
-plt.plot(s1.DataFrame["time"], A1)
-plt.plot(s1.DataFrame["time"], A2)
 plt.plot(s1.DataFrame["time"], s1.DataFrame["Alloc"])
-plt.legend([str(50),str(100),str(1)])
 plt.show()
